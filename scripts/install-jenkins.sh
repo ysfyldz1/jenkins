087ed185fd46912a4720a6479aa57910c4f0c615
ysfyldz1 <77621154+ysfyldz1@users.noreply.github.com>
2021-10-24 14:30:02 +0300

    Update install-jenkins.sh

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ 
+sudo apt install openjdk-11-jdk
+sudo apt install jenkins
+sudo apt  install docker.io
+
+mkdir -p /var/jenkins_home
+chown -R 1000:1000 /var/jenkins_home/
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+echo 'Jenkins installed'
+echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
